469a2ccc9cc3394940438b63917969ccf32cb620
Tim West <gethe88@gmail.com>
2017-12-31 11:15:15 -0600

    Initail commit

---
+++ index.js
@@ -0,0 +1,77 @@
+'use babel'
+
+let int = '\\d+'
+let decimal = '\\.' + int
+let float = '(?:' + int + decimal + '|' + int + '|' + decimal + ')'
+
+let comma = '\\s*,\\s*'
+let commaMaybe = '\\s*,?\\s*'
+let ps = '\\(\\s*'
+let pe = '\\s*\\)'
+
+// decimalColorArgs = \(\s*((?:\d+\.\d+|\d+|\.\d+))\s*,\s*((?:\d+\.\d+|\d+|\.\d+))\s*,\s*((?:\d+\.\d+|\d+|\.\d+))\s*,\s*((?:\d+\.\d+|\d+|\.\d+))\s*\)
+let decimalColorArgs = ps +
+  '(' + float + ')' +
+  comma +
+  '(' + float + ')' +
+  comma +
+  '(' + float + ')' +
+  commaMaybe +
+  '(' + float + ')?' +
+pe
+
+function decimalColorHandler (match, expression, context) {
+  console.log('wow:create_color', this, match, expression)
+  this.red = context.readFloat(match[1]) * 255
+  this.green = context.readFloat(match[2]) * 255
+  this.blue = context.readFloat(match[3]) * 255
+
+  let alpha = match[4] == null ? 1 : match[4]
+  this.alpha = context.readFloat(alpha)
+}
+
+let decimalColorFunctions = {
+  // color object methods
+  create_color: 'CreateColor',
+  set_rgb: 'SetRGB',
+  set_rgba: 'SetRGBA',
+
+  // frame methods
+  set_backdrop_color: 'SetBackdropColor',
+  set_backdrop_border_color: 'SetBackdropBorderColor',
+
+  // texture methods
+  set_color_texture: 'SetColorTexture',
+  set_vertex_color: 'SetVertexColor',
+
+  // font methods
+  set_text_color: 'SetTextColor'
+}
+
+let expressions = []
+
+for (let func in decimalColorFunctions) {
+  console.log(func, decimalColorFunctions[func])
+  expressions.push({
+    name: 'wow:' + func,
+    regexpString: decimalColorFunctions[func] + decimalColorArgs,
+    scopes: ['lua'],
+    handle: decimalColorHandler
+  })
+}
+
+export default {
+
+  provideColorExpressions: () => {
+    console.log('provideColorExpressions')
+    return {expressions}
+  },
+
+  activate (state) {
+    console.log('PigmentsWow was activated!')
+  },
+
+  deactivate () {
+    console.log('PigmentsWow was deactivated!')
+  }
+}
---
+++ test.lua
@@ -0,0 +1,28 @@
+-- Color object
+local color = _G.CreateColor(0, 0, 1, 1)
+color:SetRGB(1, 0, 0)
+color:SetRGBA(1, 0, 0, 0.5)
+
+-- frame methods
+local frame = _G.CreateFrame("Frame")
+frame:SetBackdropColor(0, 0, 0)
+frame:SetBackdropColor(0, 0, 0, 0.5)
+
+frame:SetBackdropBorderColor(0, 1, 0)
+frame:SetBackdropBorderColor(0, 1, 0, 0.5)
+
+-- texture methods
+local texture = frame:CreateTexture()
+texture:SetColorTexture(1, 0.5, 0)
+texture:SetColorTexture(1, 0.5, 0, .5)
+
+texture:SetVertexColor(0.5, 1, 0)
+texture:SetVertexColor(0.5, 1, 0, 0.5)
+
+texture:SetGradient("VERTICAL", 0, 0, 0, 1, 1, 1)
+texture:SetGradient("VERTICAL", 1, 1, 1, 0, 0, 0)
+
+-- font methods
+local font = frame:CreateTexture()
+font:SetTextColor(1, 1, 0)
+font:SetTextColor(1, 1, 0, 0.5)
